5c5d9654932749dc6ebca3ea4b8d72a9e4d38bbc
HoJoon Lim <ujuj0202@gmail.com>
2024-04-23 23:10:08 +0900

    #25 [FEAT] Product, Cart 엔티티 구현 (#26)

---
+++ src/main/java/com/example/project/domain/product/model/Product.java
@@ -0,0 +1,59 @@
+package com.example.project.domain.product.model;
+
+import com.example.project.domain.common.model.BaseTime;
+import com.example.project.domain.user.model.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product extends BaseTime {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long productId;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  @Column(nullable = false, columnDefinition = "varchar(255)")
+  private String productName;
+
+  @Column(nullable = false, columnDefinition = "text")
+  private String productContent;
+
+  @Column(nullable = false, columnDefinition = "varchar(255)")
+  @Enumerated(EnumType.STRING)
+  private ProductStatus productStatus;
+
+  @Column(nullable = false, columnDefinition = "int unsigned")
+  private int price;
+
+  @Column(nullable = false, columnDefinition = "int unsigned")
+  private int stock;
+
+  @Builder
+  public Product(Long productId, User user, String productName, String productContent,
+      ProductStatus productStatus, int price, int stock) {
+    this.productId = productId;
+    this.user = user;
+    this.productName = productName;
+    this.productContent = productContent;
+    this.productStatus = productStatus;
+    this.price = price;
+    this.stock = stock;
+  }
+}
---
+++ src/main/java/com/example/project/domain/product/model/ProductStatus.java
@@ -0,0 +1,5 @@
+package com.example.project.domain.product.model;
+
+public enum ProductStatus {
+  DELETED, ON_SALE, SOLD_OUT
+}
